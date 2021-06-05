d974fb137d7361ea6804282167e0b3c7fe7d3734
Oleh Melnychuk <olehmelnychuk1999@gmail.com>
2021-06-05 21:58:09 +0300

    init redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { rootReducer } from './redux/rootReducer';
+import { Provider } from 'react-redux';
+
+const  store = createStore(rootReducer);
+const app = () => {
+  <Provider store={store}>
+    <App />
+  </Provider>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <app />
+,
   document.getElementById('root')
 );
 
